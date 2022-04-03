49a80a258b634d619c754d26ddc9ddc5abccf258
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-03 14:57:12 +0200

    Added docstring to central

--- src/differentiate.py
+++ src/differentiate.py
@@ -46,6 +46,23 @@ def backward(order:int, f:'function', x:float, h:float, exact:bool = False) -> f
 
 
 def central(order:int, f:'function', x:float, h:float, exact:bool = False) -> float: 
+    """Use central finite difference formula of order `order` to compute the derivative of `f` at `x`.
+
+    Args:
+        order (int): Order of the derivate. (first, second ...).
+        f (function): Function for which the derivative will be computed. 
+            Only one argument will be passed to it, `x`. 
+        x (float): Point at which the derivative will be computed.
+        h (float): Error of the approximation # TODO: Is it the error?
+        exact (bool, optional): Set the precision of the method. 
+            If False, floating comma numbers are used. Set to True to use long numbers. Defaults to False.
+
+    Note:
+        See more of `exact` parameter in https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.comb.html
+
+    Returns:
+        float: Derivative of order `order` of `f` evaluated at `x`. 
+    """  
     return sum( [ (-1)**(k) * sci.comb(order, k, exact) * f(x - (order/2 - k)*h) for k in  range(order+1)] ) / h**2
 
 

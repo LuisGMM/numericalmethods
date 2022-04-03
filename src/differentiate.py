35ad0bf6a9b736d8bc71ecffe232b7afe336dd23
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-03 14:45:11 +0200

    added exact arg for precision in central

--- src/differentiate.py
+++ src/differentiate.py
@@ -11,8 +11,8 @@ def backward(order:int, f:'function', x:float, h:float, exact:bool = False) -> f
     return sum( [ (-1)**(k) * sci.comb(order, k, exact) * f(x - k*h) for k in  range(order+1)] ) / h**2
 
 
-def central(order:int, f:'function', x:float, h:float) -> float: 
-    return sum( [ (-1)**(k) * sci.comb(order, k) * f(x - (order/2 - k)*h) for k in  range(order+1)] ) / h**2
+def central(order:int, f:'function', x:float, h:float, exact:bool = False) -> float: 
+    return sum( [ (-1)**(k) * sci.comb(order, k, exact) * f(x - (order/2 - k)*h) for k in  range(order+1)] ) / h**2
 
 
 

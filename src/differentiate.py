92907580f7d264e601cecc21d0b35dd5a4d6d10d
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-03 14:22:53 +0200

    presented backward

--- src/differentiate.py
+++ src/differentiate.py
@@ -7,3 +7,7 @@ import numpy as np
 
 def forward(order:int, f:'function', x:float, h:float) -> float: 
     raise NotImplementedError()
+
+
+def backward(order:int, f:'function', x:float, h:float) -> float: 
+    raise NotImplementedError()

b771d1ca7a3616f746fb39e09bbb294435b71430
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-03 14:44:17 +0200

    added exact arg for precision in forward

--- src/differentiate.py
+++ src/differentiate.py
@@ -1,12 +1,10 @@
 
-import math 
 
-import numpy as np
 import scipy.special as sci
 
 # Tested
-def forward(order:int, f:'function', x:float, h:float) -> float:     
-    return sum( [ (-1)**(order-k) * sci.comb(order, k) * f(x + k*h) for k in  range(order+1)] ) / h**2
+def forward(order:int, f:'function', x:float, h:float, exact:bool = False) -> float:     
+    return sum( [ (-1)**(order-k) * sci.comb(order, k, exact) * f(x + k*h) for k in  range(order+1)] ) / h**2
 
 
 def backward(order:int, f:'function', x:float, h:float) -> float: 

afdf1aedf4b2ec276558b0ff7a693d8d01a5cc7d
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-03 14:41:57 +0200

    Update differentiate.py

--- src/differentiate.py
+++ src/differentiate.py
@@ -12,6 +12,7 @@ def forward(order:int, f:'function', x:float, h:float) -> float:
 def backward(order:int, f:'function', x:float, h:float) -> float: 
     return sum( [ (-1)**(k) * sci.comb(order, k) * f(x - k*h) for k in  range(order+1)] ) / h**2
 
+
 def central(order:int, f:'function', x:float, h:float) -> float: 
     return sum( [ (-1)**(k) * sci.comb(order, k) * f(x - (order/2 - k)*h) for k in  range(order+1)] ) / h**2
 

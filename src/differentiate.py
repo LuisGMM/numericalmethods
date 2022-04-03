537539851d0e4b2236d3dc497de2bb407d26b4b2
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-03 14:40:41 +0200

    added backward

--- src/differentiate.py
+++ src/differentiate.py
@@ -5,13 +5,12 @@ import numpy as np
 import scipy.special as sci
 
 # Tested
-def forward(order:int, f:'function', x:float, h:float) -> float: 
-    
-    return sum( [ (-1)**(order-k) * sci.comb(order, k) * f(x+k*h) for k in  range(order+1)] ) / h**2
+def forward(order:int, f:'function', x:float, h:float) -> float:     
+    return sum( [ (-1)**(order-k) * sci.comb(order, k) * f(x + k*h) for k in  range(order+1)] ) / h**2
 
 
 def backward(order:int, f:'function', x:float, h:float) -> float: 
-    raise NotImplementedError()
+    return sum( [ (-1)**(k) * sci.comb(order, k) * f(x - k*h) for k in  range(order+1)] ) / h**2
 
 def central(order:int, f:'function', x:float, h:float) -> float: 
     raise NotImplementedError()

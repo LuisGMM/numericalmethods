d89145abbba4526df40700b6e8b6ac43836a71ae
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-04 18:08:14 +0200

    corrected central

--- src/differentiate.py
+++ src/differentiate.py
@@ -63,7 +63,7 @@ def central(order:int, f:'function', x:float, h:float, exact:bool = False) -> fl
     Returns:
         float: Derivative of order `order` of `f` evaluated at `x`. 
     """  
-    return sum( [ (-1)**(k) * sci.comb(order, k, exact) * f(x - (order/2 - k)*h) for k in  range(order+1)] ) / h**2
+    return sum( [ (-1)**(k) * sci.comb(order, k, exact) * f(x - (order/2 - k)*h) for k in  range(order+1)] ) / h**order
 
 
 

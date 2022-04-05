51a52b236dda59f36dc7f6d451cd9e0df3987f4f
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 11:51:56 +0200

    Update test_roots.py

--- tests/test_roots.py
+++ tests/test_roots.py
@@ -1,3 +1,7 @@
 from charliepy.roots import newton
 
-def test_newton():...
+def test_newton():
+    ans = 1
+    f = lambda x: x**2 -1
+    f_dev = lambda x: 2*x
+    assert (newton(err=1e10, f=f, f_dev=f_dev, x0=5), 10) == ans

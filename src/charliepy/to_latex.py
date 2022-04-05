fcafcba1d705c66bd2130e55cde9e317e138648d
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 12:54:13 +0200

    Update to_latex.py

--- src/charliepy/to_latex.py
+++ src/charliepy/to_latex.py
@@ -1,23 +1,28 @@
 
 import pandas as pd
 
+from typing import List
 #TODO: Account doc | & functionality for the kwargs
 
-def to_latex(col_names: 'list[str, ...]', cols: 'list[list, ...]', **kwargs) -> str:
+def to_latex(col_names: 'List[str, ...]', cols: 'List[List, ...]', **kwargs) -> str:
     """High-level wrapper of pandas.DataFrame.to_latex with limited options.
+    
     Zips the strings in col_names with the sublists in cols and generates a LaTeX table in string format 
+    
     Args:
         col_names (list[str]): The names of each data column in cols. They will appear in the first row.
         cols (list[list]): Contains lists, each a supposed column of the table
+    
     Returns:
         str: Latex table.
+    
+    Examples:
+        >>> names = ['luis', 'pablo']
+        >>> cols = [list(range(10)), list(range(10))]
+        >>> to_latex(names, cols)
     """
     return pd.DataFrame(dict(zip(col_names, cols))).to_latex(**kwargs)
 
 
 if __name__ == '__main__':
-
-    names = ['luis', 'pablo']
-    cols = [list(range(10)), list(range(10))]
-
-    print(to_latex(names, cols))
+    pass

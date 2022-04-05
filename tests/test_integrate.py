347737b9a7a97a3615c1744e1ea834f46f3f62f6
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 12:31:14 +0200

    Update test_integrate.py

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -9,10 +9,10 @@ def test_composite_trapezoid():
     ans = 1.4887
     f = lambda x: np.exp(-x**2)
     
-    assert round( composite_trapezoid(f_=f, a=-1, b=1, n=10), 4) == ans
+    assert round(composite_trapezoid(f_=f, a=-1, b=1, n=10), 4) == ans
 
 def composite_simpson():
     ans = 1.49367
     f = lambda x: np.exp(-x**2)
     
-    assert round( composite_simpson(f_=f, a=-1, b=1, n=10), 5) == ans
+    assert round(composite_simpson(f_=f, a=-1, b=1, n=10), 5) == ans

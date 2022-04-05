6ee97a0f7604a46e24b848ad09babc47c3271651
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 12:30:23 +0200

    Update test_integrate.py

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -5,6 +5,10 @@ from charliepy.integrate import composite_simpson, composite_trapezoid
 
 
 
-def test_composite_trapezoid(): ...
+def test_composite_trapezoid(): 
+    ans = 1.4887
+    f = lambda x: np.exp(-x**2)
+    
+    assert round( composite_trapezoid(f_=f, a=-1, b=1, n=10), 4) == ans
 
 def composite_simpson():...

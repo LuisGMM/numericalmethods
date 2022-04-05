e7de98270c7f7afb6e043376691d931f0ac85387
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 12:22:53 +0200

    Update test_roots.py

--- tests/test_roots.py
+++ tests/test_roots.py
@@ -11,9 +11,14 @@ def test_newton_f_and_f_dev():
 
     assert round(newton(err=1e-10, f=f, f_dev=f_dev, x0=5), 10) == ans
 
-def test_newton_f_dev_and_integrator():
-    ans = 1.6448536269514884
+def test_newton_f_dev_and_integrator_composite_trapezoid():
     ans = 1.6448536269886083
     f_dev = lambda x: 1 / np.sqrt(2*np.pi) * np.exp(-x**2 / 2)
 
     assert newton(err=1e-5, f_dev=f_dev, integrator=composite_trapezoid, c=-0.45) == ans
+
+def test_newton_f_dev_and_integrator_composite_simpson():
+    ans = 1.6448536269514884
+    f_dev = lambda x: 1 / np.sqrt(2*np.pi) * np.exp(-x**2 / 2)
+
+    assert newton(err=1e-5, f_dev=f_dev, integrator=composite_simpson, c=-0.45) == ans

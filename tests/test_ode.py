d3ce9640ac01b490d302c43013392bc5ea556603
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 20:30:28 +0200

    Update test_ode.py

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -6,5 +6,8 @@ def test_euler_explicit():
     
     ans = [1.0, 1.1, 1.22, 1.3620, 1.5282, 1.7210, 1.9431, 2.1974, 2.4872, 2.8159]
     f = lambda x, y: x+y
-    
+
     assert [round(sol, 4) for sol in euler_explicit(f=f, y0=1, t0=0, t=1, h=0.1)] == ans
+
+
+def test_euler_implicit():...

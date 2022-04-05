6474041db009e015b37489e9b144353f0f326de1
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 11:54:52 +0200

    Update test_roots.py

--- tests/test_roots.py
+++ tests/test_roots.py
@@ -4,4 +4,4 @@ def test_newton():
     ans = 1
     f = lambda x: x**2 -1
     f_dev = lambda x: 2*x
-    assert (newton(err=1e10, f=f, f_dev=f_dev, x0=5), 10) == ans
+    assert round(newton(err=1e-10, f=f, f_dev=f_dev, x0=5), 10) == ans

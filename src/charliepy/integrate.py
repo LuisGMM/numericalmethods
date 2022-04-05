fe51ce9d3315c9cbf2be3a4134acb0f34b7db0d2
Luis_Grau_IL <emaildemiscuentas@gmail.com>
2022-04-05 19:46:31 +0200

    Update integrate.py

--- src/charliepy/integrate.py
+++ src/charliepy/integrate.py
@@ -5,8 +5,7 @@ from typing import Callable
 import numpy as np
 
 
-
-def composite_trapezoid(f_:'Callable[float]', a:float, b:float, n:float)-> float:
+def composite_trapezoid(f_: 'Callable[float]', a: float, b: float, n: float) -> float:
     """Computes the analitical solution of the integral of f from a to b 
     following the composite trapezoidal rule. 
 
@@ -18,7 +17,7 @@ def composite_trapezoid(f_:'Callable[float]', a:float, b:float, n:float)-> float
 
     Returns:
         float: Numerical solution of the integral.
-    """    
+    """
     x = np.linspace(a, b, n + 1)
     f = f_(x)
     h = (b - a) / (n)
@@ -26,7 +25,7 @@ def composite_trapezoid(f_:'Callable[float]', a:float, b:float, n:float)-> float
     return h/2 * sum(f[:n] + f[1:n+1])
 
 
-def composite_simpson(f_:'Callable[float]', a:float, b:float, n:float)-> float:
+def composite_simpson(f_: 'Callable[float]', a: float, b: float, n: float) -> float:
     """Computes the analitical solution of the integral of f from a to b 
     following the composite Simpson's 1/3 rule. 
 
@@ -38,9 +37,9 @@ def composite_simpson(f_:'Callable[float]', a:float, b:float, n:float)-> float:
 
     Returns:
         float: Numerical solution of the integral.
-    """    
+    """
     x = np.linspace(a, b, n+1)
     f = f_(x)
     h = (b - a) / (n)
-    
-    return (h/3) * ( f[0] + 2*sum(f[2:n-1:2]) + 4*sum(f[1:n:2]) + f[n] )
+
+    return (h/3) * (f[0] + 2*sum(f[2:n-1:2]) + 4*sum(f[1:n:2]) + f[n])

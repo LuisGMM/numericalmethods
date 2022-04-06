33a0966ddb7e3c512f68e3377a70df3c6c1ceb59
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-06 10:14:36 +0200

    implemented newton_horner

--- src/numericalmethods/interpolate.py
+++ src/numericalmethods/interpolate.py
@@ -2,4 +2,19 @@
 import numpy as np
 
 
-def newton_horner(x: np.ndarray, y: np.ndarray) -> np.ndarray:...
+def newton_horner(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+
+    LEN = len(y)
+    matrix = np.zeros((LEN, LEN))
+
+    # matrix column -> j
+    for j in range(LEN):
+        if j == 0:
+            matrix[:, 1] = y
+
+        # matrix row -> i
+        for i in range(LEN-j):
+
+            matrix[i, j] = (matrix[i+1, j] - matrix[i, j]) / (x[i+1] - x[i])
+
+    return matrix[0]

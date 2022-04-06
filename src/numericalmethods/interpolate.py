c0db1aa98f33850f9d93abe125237c72d8d11402
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-06 10:16:22 +0200

    Update interpolate.py

--- src/numericalmethods/interpolate.py
+++ src/numericalmethods/interpolate.py
@@ -3,7 +3,21 @@ import numpy as np
 
 
 def newton_horner(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """ Computes Newton interpolation polynomial by Horner's algorithm for some given coordinates.
 
+    `x` and `y` must have the same length.
+
+    Args:
+        x(np.ndarray): x coordinates of the points.
+        y(np.ndarray): y coordinates of the points.
+
+    Raises:
+        ValueError: If `x` and `y` are not of the same length. 
+
+    Returns:
+        list: with the polynomial coefficients of the Newton interpolation.
+
+    """
     if len(x)!=len(y): 
         raise ValueError('x and y must be the same length.')
 

0f0afe778bddde7809a23fcc9baec09c063d4879
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-17 18:39:11 +0200

    Update to_base.py

--- src/numericalmethods/to_base.py
+++ src/numericalmethods/to_base.py
@@ -1,17 +1,24 @@
 
 
 def to_base(base:int, number:int) -> str:
+    """ Changes an integer from decimal base to other base. 
 
+    Args:
+        base (int): New base of the number to be converted.
+        number (int): Number to be converted to the new base.
 
-    cociente = number // base
+    Returns:
+        str: A string representation of the number in the new base.
+    """
+    quotient = number // base
     remainder = number % base
     ans = str(remainder)
 
-    while (cociente >= base):
+    while (quotient >= base):
 
-        remainder = cociente % base
-        cociente //= base
+        remainder = quotient % base
+        quotient //= base
         ans = str(remainder) + ans
 
     else: 
-        return str(cociente%base) + ans
+        return str(quotient%base) + ans

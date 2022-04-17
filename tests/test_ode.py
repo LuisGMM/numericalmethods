605c89fae99c1aba5d7f2745c3a687ee66ca1784
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-17 22:10:18 +0200

    Update test_ode.py

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -6,7 +6,7 @@ def test_euler_explicit():
 
     ans = [1.0, 1.1, 1.22, 1.3620, 1.5282, 1.7210, 1.9431, 2.1974, 2.4872, 2.8159]
 
-    assert [round(sol, 4) for sol in euler_explicit(f=lambda x, y: x+y, y0=1, t0=0, t=1, h=0.1)] == ans
+    assert [round(sol, 4) for sol in euler_explicit(f=lambda x, y: x+y, y0=1, t0=0, t=1, h=0.1)][:-1] == ans
 
 
 def test_euler_implicit_without_dependance_on_y():
@@ -18,7 +18,7 @@ def test_euler_implicit_without_dependance_on_y():
 
 def test_runge_kutta4():
 
-    ans = [1, 1.11034167, 1.24280514, 1.39971699, 1.58364848, 1.79744128,
+    ans = [1.0, 1.11034167, 1.24280514, 1.39971699, 1.58364848, 1.79744128,
            2.04423592, 2.32750325, 2.65107913, 3.01920283, 3.43655949]
 
-    assert list(rk4(lambda y, t : y + t, y0=0, t0=0, t=1, h=0.1)) == ans
+    assert [round(i, 4) for i in rk4(lambda y, t : y + t, y0=1, t0=0, t=1, h=0.1)] == [round(i, 4) for i in ans]

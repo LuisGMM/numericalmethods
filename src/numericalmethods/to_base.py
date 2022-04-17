dd89fb6e14fd99cd092049d272b163fd11857418
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-17 18:38:48 +0200

    Update to_base.py

--- src/numericalmethods/to_base.py
+++ src/numericalmethods/to_base.py
@@ -1,3 +1,17 @@
 
 
 def to_base(base:int, number:int) -> str:
+
+
+    cociente = number // base
+    remainder = number % base
+    ans = str(remainder)
+
+    while (cociente >= base):
+
+        remainder = cociente % base
+        cociente //= base
+        ans = str(remainder) + ans
+
+    else: 
+        return str(cociente%base) + ans

30dce0dcf7e21f76d382f4a9c6e52145169c8f3d
LuisGMM <emaildemiscuentas@gmail.com>
2022-05-04 17:41:03 +0200

    Update pde.py

--- src/numericalmethods/pde.py
+++ src/numericalmethods/pde.py
@@ -146,3 +146,7 @@ def crank_nik_parabolic(h: float, k: float, x0: float, xf: float, t0: float, tf:
         sol[:, ti] = m@sol[:, ti-1]
 
     return sol, x, t
+
+
+def theta_parabolic(theta: float, h: float, k: float, x0: float, xf: float, t0: float, tf: float, u0: Callable) -> Tuple[np.ndarray]:
+    pass

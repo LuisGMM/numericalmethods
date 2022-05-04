e21bd762aa4b297ec49723b11e2d466851fab373
LuisGMM <emaildemiscuentas@gmail.com>
2022-05-04 11:00:22 +0200

    Update pde.py

--- src/numericalmethods/pde.py
+++ src/numericalmethods/pde.py
@@ -8,3 +8,6 @@ def parabollic(theta, h: float, k: float, x0: float, xf: float, tf: float, u0: n
 
     x = np.arange(x0, xf+h, h)
     t = np.arange(0, tf+h, h)
+
+    LEN_X = len(x)
+    LEN_T = len(t)

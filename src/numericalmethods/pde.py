09a1941ebe4584b3ec07491179c0e75268415544
LuisGMM <emaildemiscuentas@gmail.com>
2022-05-04 11:00:16 +0200

    Update pde.py

--- src/numericalmethods/pde.py
+++ src/numericalmethods/pde.py
@@ -4,3 +4,7 @@ import numpy as np
 
 def parabollic(theta, h: float, k: float, x0: float, xf: float, tf: float, u0: np.ndarray) -> np.ndarray:
     
+    sigma = k/h**2
+
+    x = np.arange(x0, xf+h, h)
+    t = np.arange(0, tf+h, h)

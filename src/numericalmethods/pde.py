e73307358ff721b46cda18cd0fc5abcc8f73768b
LuisGMM <emaildemiscuentas@gmail.com>
2022-05-04 11:16:55 +0200

    Update pde.py

--- src/numericalmethods/pde.py
+++ src/numericalmethods/pde.py
@@ -2,7 +2,8 @@
 import numpy as np
 
 
-def __tridiag(v1: float, v2: float, v3: float, k1: int = -1, k2: int = 0, k3: int = 1) -> np.ndarray:
+def __tridiag(v1: float, v2: float, v3: float, N: int, k1: int = -1, k2: int = 0, k3: int = 1) -> np.ndarray:
+
     return np.diag(v1, k1) + np.diag(v2, k2) + np.diag(v3, k3)
 
 

82b7c1d023b6c2fd635af3c76275a29e4d96800e
LuisGMM <emaildemiscuentas@gmail.com>
2022-05-05 08:49:46 +0200

    Update differentiate.py

--- src/numericalmethods/differentiate.py
+++ src/numericalmethods/differentiate.py
@@ -9,7 +9,7 @@ def forward(order: int, f: Callable[[float], float], x: float, h: float, exact: 
 
     Args:
         order (int): Order of the derivate. (first, second ...).
-        f (Callable): Function for which the derivative will be computed.
+        f (Callable[[float], float]): Function for which the derivative will be computed.
             Only one argument will be passed to it, `x`.
         x (float): Point at which the derivative will be computed.
         h (float): Error of the approximation # TODO: Is it the error?
